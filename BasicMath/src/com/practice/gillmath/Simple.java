c6fba74db6ceabb5f44c790e1565315e39e74950
Shuaib Gill <shuaibg77@gmail.com>
2018-09-01 22:18:27 -0400

    initial commit

---
+++ BasicMath/src/com/practice/gillmath/Simple.java
@@ -0,0 +1,26 @@
+package com.practice.gillmath;
+
+public class Simple {
+	
+	private Integer add(int x, int y)
+	{	return x + y;	}
+	
+	private Integer subtract(int x, int y)
+	{	return x - y;	}
+	
+	private void getOSDetails()
+	{
+		System.out.println(System.getProperty("os.name"));
+		System.out.println(System.getProperty("os.version"));
+	}
+
+	public static void main(String[] args) {
+		Simple numbers = new Simple();
+		int number1 = -4, number2 = 14;
+		
+		System.out.println(numbers.add(number1, number2));
+		System.out.println(numbers.subtract(number1, number2));
+		numbers.getOSDetails();
+	}
+
+}
